7f392cb7b8e74817d8e2e41fcb19a654afdfadb8
Ricardo Cantó <rifcaab@gmail.com>
2018-11-19 00:12:26 +0100

    esborrant una coma

--- templates/index_template.php
+++ templates/index_template.php
@@ -6,7 +6,7 @@
             "href" : "{{ path_for('index')}}",
       
             "links" : [
-                {"rel" : "collection", "href" : "{{ path_for('index') }}","prompt":"Articulos"},
+                {"rel" : "collection", "href" : "{{ path_for('index') }}","prompt":"Articulos"}
             ]
             }
 }	
