343d033d9ae954818a327fe240291dc1867c1a23
Ricardo Cantó <rifcaab@gmail.com>
2018-11-18 18:08:42 +0100

    refent tota la estructura del projecte

---
+++ templates/root_template.php
@@ -0,0 +1,12 @@
+{ "collection" :
+    {
+        "title" : "Multimedia Database",
+            "type" : "index",
+            "version" : "1.0",
+            "href" : "{{ path_for('root')}}",
+      
+            "links" : [
+                {"rel" : "collection", "href" : "{{ path_for('articulos') }}","prompt":"Articles"},
+            ]
+            }
+}	
